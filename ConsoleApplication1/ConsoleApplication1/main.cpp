047d90afa98b830ec6cc8d7c1d405d06fe61d437
David Maslin <debugnus@gmail.com>
2017-06-21 21:46:08 +0930

    Success

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,62 +1,40 @@
 #include <iostream>
-#include <vector>
-
-// TODO funtion for calculation!
-// FUNCTIONS FOR EVERYTHING
-// prototypes up top functions down bottom
-
-void Init();
-void GetNumber();
-void CompareIntegers();
-void PrintResult();
 
 int main() {
 
-	Init();
-
-	while (std::cin >> Init) {
-
-		CompareIntegers();
-		PrintResult();
-
-		return 0;
-	}
-}
-
-
-		// Class or  Struct ?
-
-void Init() {
-
-	//std::vector<double> numbers;
-	//int InfiniteLoopCheck = 0;
-
+	// initialise variables
 	double Number = 0.0;
 	double Largest = 0.0;
 	double Smallest = 0.0;
-}
 
+	std::cout << "Give me a number: ";
+	std::cin >> Number;
 
-void GetNumber() {
-	double Number = 0;
-	std::cout << "Give me 1 integer: ";
-	
-	
-}
+	// change to first seen number
+	Largest = Number;
+	Smallest = Number;
 
-void CompareIntegers() {
-	// compare integers
-	if (Number > Smallest && Number > Largest) {
-		Largest = Number;
-	}
-	else if (Number < Largest && Number < Smallest) {
-		Smallest = Number;
-	}
-}
-
-void PrintResult() {
-	// print out largest and smallest so far
 	std::cout << "\n" << Largest << " is the largest number so far!\n"
 		<< Smallest << " is the smallest number so far!\n";
 
+	std::cout << "Give me a number: ";
+	while (std::cin >> Number) {
+
+
+		if (Number > Smallest && Number > Largest) {
+			Largest = Number;
+		}
+		else if (Number < Largest && Number < Smallest) {
+			Smallest = Number;
+		}
+
+
+		std::cout << "\n" << Largest << " is the largest number so far!\n"
+			<< Smallest << " is the smallest number so far!\n";
+
+		std::cout << "Give me a number: ";
+
+	}
+
+	return 0;
 }
